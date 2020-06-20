9e854a511d197baf499eb3d36803409a85dee393
augustolimads <augustolimads@gmail.com>
2020-06-20 19:02:26 -0300

    adiciona variables styles

---
+++ src/styles/variables.js
@@ -0,0 +1,81 @@
+// ==================================================
+// Variables
+// 1. Sizes
+// 2. Colors
+// 3. zIndex
+// 4. Responsive
+// 5. Misc
+// ==================================================
+
+// --------------------------------------------------
+// 1. Spaces
+// --------------------------------------------------
+
+export const Size = {
+    xxs: `.8rem`,
+    xs: `1rem`,
+    sm: `1.5rem`,
+    md: `2.5rem`,
+    lg: `4rem`,
+    xlg: `6.5rem`,
+    xxlg: `10.5rem`,
+}
+
+// --------------------------------------------------
+// 2. Colors
+// --------------------------------------------------
+
+export const Color = {
+    white: `rgba(255, 255, 255, .9)`,
+    black: `rgba(15, 15, 15, .95)`,
+    whiteSolid: `#ffffff`,
+    blackSolid: `#000000`,
+    blue: `#187888`,
+    blueLight: `#a1ecf7`,
+    grayDarker: `#1d1d1d`,
+    grayDark: `#2d2d2d`,
+    gray: `#757575`,
+    grayLight: `#aaaaaa`,
+    grayLighter: `#efefef`,
+    grayWhite: `#f1f1f1`,
+    yellow: `#e6af05`,
+}
+
+// --------------------------------------------------
+// 3. zIndex
+// --------------------------------------------------
+
+export const ZIndex = {
+    bg: `-1`,
+    default: `1`,
+    footer: `5`,
+    header: `5`,
+}
+
+// --------------------------------------------------
+// 4. Responsive
+// --------------------------------------------------
+export const MaxSize = {
+    md: `750px`,
+    lg: `1080px`,
+}
+// --------------------------------------------------
+// 5. Misc
+// --------------------------------------------------
+
+export const Transition = {
+    default: `.25s`,
+}
+
+export const BorderRadius = {
+    box: `2px`,
+}
+
+export const Border = {
+    box: `1px solid var(--borderColor)`,
+    default: `1px solid var(--borderColor)`,
+}
+
+export const BoxShadow = {
+    box: `0 1px 3px rgba(250, 250, 250, .12)`,
+}
