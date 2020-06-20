289cb14d08c7022bb544c4c12ae6a42afd45fa84
augustolimads <augustolimads@gmail.com>
2020-06-20 18:34:24 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DarkMode from './components/dark-mode'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>aqui pega</h1>
+      <DarkMode />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/dark-mode/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DarkMode = () => {
+    return (<h1>Teste</h1>)
+}
+
+export default DarkMode
